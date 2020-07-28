a7c03d2fe369baeb89595cc0460d266166bce664
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 13:22:42 +0530

    Add files via upload

--- src/model/User.java
+++ src/model/User.java
@@ -1,2 +1,41 @@
 package model;
 
+import java.sql.Date;
+import java.time.*;
+
+public class User{
+	private String  email;
+	
+	private String  Date;
+	
+	private String  password;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getDate() {
+		return Date;
+	}
+
+	public void setDate(String date) {
+		Date = date;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+	
+	
+	
+}
