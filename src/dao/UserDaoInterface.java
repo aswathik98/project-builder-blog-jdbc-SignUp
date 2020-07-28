642bc7f4d26e00b4246090abb5b395b5fac221f0
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-28 13:22:02 +0530

    Add files via upload

--- src/dao/UserDaoInterface.java
+++ src/dao/UserDaoInterface.java
@@ -1,2 +1,12 @@
 package dao;
 
+import java.io.IOException;
+import java.sql.SQLException;
+
+import model.*;
+
+interface UserDaoInterface{
+	public int signUp(User user) throws  ClassNotFoundException, SQLException, IOException ;
+	public boolean login(User user) throws  ClassNotFoundException, SQLException, IOException ;
+}
+
